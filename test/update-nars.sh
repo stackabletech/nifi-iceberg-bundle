9f8a334540f50a4ff0fed8893507d6d35dcd581d
Nick Larsen <nick.larsen@stackable.tech>
2025-09-23 14:39:31 +0200

    test: Parameterise namespace

--- test/update-nars.sh
+++ test/update-nars.sh
@@ -3,17 +3,19 @@ set -e
 
 # Run from repo root directory!
 
+NAMESPACE="${1:-nifi}"
+
 mvn -D nifi.version=2.4.0 -D cyclonedx.skip=true -D skipTests clean package
 
-kubectl -n nifi wait --for=condition=Ready pod/nifi-node-default-0
+kubectl -n "$NAMESPACE" wait --for=condition=Ready pod/nifi-node-default-0
 
 echo "Uploading nar files..."
-kubectl -n nifi cp -c nifi ./nifi-iceberg-services-api-nar/target/nifi-iceberg-services-api-nar-*.nar nifi-node-default-0:/stackable/userdata/nifi-processors/
-kubectl -n nifi cp -c nifi ./nifi-iceberg-services-nar/target/nifi-iceberg-services-nar-*.nar nifi-node-default-0:/stackable/userdata/nifi-processors/
-kubectl -n nifi cp -c nifi ./nifi-iceberg-processors-nar/target/nifi-iceberg-processors-nar-*.nar nifi-node-default-0:/stackable/userdata/nifi-processors/
+kubectl -n "$NAMESPACE" cp -c nifi ./nifi-iceberg-services-api-nar/target/nifi-iceberg-services-api-nar-*.nar nifi-node-default-0:/stackable/userdata/nifi-processors/
+kubectl -n "$NAMESPACE" cp -c nifi ./nifi-iceberg-services-nar/target/nifi-iceberg-services-nar-*.nar nifi-node-default-0:/stackable/userdata/nifi-processors/
+kubectl -n "$NAMESPACE" cp -c nifi ./nifi-iceberg-processors-nar/target/nifi-iceberg-processors-nar-*.nar nifi-node-default-0:/stackable/userdata/nifi-processors/
 
 echo "Uploaded nar files"
-kubectl -n nifi delete pod nifi-node-default-0
+kubectl -n "$NAMESPACE" delete pod nifi-node-default-0
 sleep 1
 echo "Waiting for Pod to get ready again"
-kubectl -n nifi wait --for=condition=Ready pod/nifi-node-default-0 --timeout 30m
+kubectl -n "$NAMESPACE" wait --for=condition=Ready pod/nifi-node-default-0 --timeout 30m
